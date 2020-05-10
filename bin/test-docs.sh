88d5f13b4cd78f360deaf4aa3173331ccf5344a0
Alexander Foremny <aforemny@posteo.de>
2020-05-10 19:02:14 +0200

    chore: Verify documentation examples

---
+++ bin/test-docs.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p (ghc.withPackages(pkgs: [ pkgs.pandoc ]))
+
+set -e
+
+to_markdown=$(cat <<EOF
+let
+  parse keep (('{':'-':'|':' ':line):lines) = line:parse True lines
+  parse keep (('-':'}':_):lines) = parse False lines
+  parse keep (line:lines) = if keep then line:parse keep lines else parse keep lines
+  parse keep [] = []
+    
+in
+interact (unlines . parse False . lines)
+EOF
+)
+
+for fn in src/**/*.elm; do
+  cat "$fn" \
+    | ghc -e "$to_markdown" \
+    | pandoc -f MARKDOWN -t MARKDOWN --filter "$(dirname $0)/test-docs.hs" > /dev/null
+done
